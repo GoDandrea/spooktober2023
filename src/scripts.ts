50036403401d27047818a9b45af802a26f6a6cc4
GoDandrea <dandrea.alves@gmail.com>
2023-09-24 13:24:22 -0300

    Add intro file with first scene dialogue outline

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,6 +1,7 @@
 import demo from "./scripts/demo.narrat";
+import intro from "./scripts/intro.narrat";
 import quest from "./scripts/quest.narrat";
 import prototype from "./scripts/prototype.narrat";
 import utilities from "./scripts/utilities.narrat";
 
-export default [demo, quest, prototype, utilities];
+export default [demo, intro, quest, prototype, utilities];
