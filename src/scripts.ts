4916b0ea14275e9eb6a21ab5b07bbd58eb5b60da
KlinsmannHengles <klinsmannhengles@gmail.com>
2023-09-27 17:38:54 -0300

    Drafting the callEx Scene

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,8 +1,9 @@
 import beach from "./scripts/beach.narrat";
+import callEx from "./scripts/callEx.narrat"
 import demo from "./scripts/demo.narrat";
 import intro from "./scripts/intro.narrat";
 import quest from "./scripts/quest.narrat";
 import prototype from "./scripts/prototype.narrat";
 import utilities from "./scripts/utilities.narrat";
 
-export default [beach, demo, intro, quest, prototype, utilities];
+export default [beach, callEx, demo, intro, quest, prototype, utilities];
