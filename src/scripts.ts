04a0de9039df0472adc9069e4e1ac3a703cb4b2f
Débora Prudêncio <debora.prudencio@gmail.com>
2023-09-29 12:14:10 -0300

    add friend panel, fix background size, remove demo

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,9 +1,7 @@
 import beach from "./scripts/beach.narrat";
-import callEx from "./scripts/callEx.narrat"
-import demo from "./scripts/demo.narrat";
+import callEx from "./scripts/callEx.narrat";
 import intro from "./scripts/intro.narrat";
-import quest from "./scripts/quest.narrat";
 import prototype from "./scripts/prototype.narrat";
 import utilities from "./scripts/utilities.narrat";
 
-export default [beach, callEx, demo, intro, quest, prototype, utilities];
+export default [beach, callEx, intro, prototype, utilities];
