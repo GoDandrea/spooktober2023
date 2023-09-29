2e9204191631082f8e9f404a84d928e00c416d76
KlinsmannHengles <klinsmannhengles@gmail.com>
2023-09-29 14:56:53 -0300

    intro to tasks scene and jump through all scenes

--- src/scripts.ts
+++ src/scripts.ts
@@ -2,6 +2,7 @@ import beach from "./scripts/beach.narrat";
 import callEx from "./scripts/callEx.narrat";
 import intro from "./scripts/intro.narrat";
 import prototype from "./scripts/prototype.narrat";
+import tasks from "./scripts/tasks.narrat";
 import utilities from "./scripts/utilities.narrat";
 
-export default [beach, callEx, intro, prototype, utilities];
+export default [beach, callEx, intro, prototype, tasks, utilities];
