45fe082bb42ceda450aa315f083fdb505634d9f4
KlinsmannHengles <klinsmannhengles@gmail.com>
2023-10-01 19:38:59 -0300

    end scenes created and config

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,6 +1,9 @@
 import beach from "./scripts/beach.narrat";
 import callEx from "./scripts/callEx.narrat";
 import climax from "./scripts/climax.narrat";
+import end_ex from "./scripts/end_ex.narrat"
+import end_friend from "./scripts/end_friend.narrat"
+import end_mother from "./scripts/end_mother.narrat"
 import intro from "./scripts/intro.narrat";
 import friend from "./scripts/friend.narrat"
 import prototype from "./scripts/prototype.narrat";
@@ -8,4 +11,4 @@ import tasks from "./scripts/tasks.narrat";
 import utilities from "./scripts/utilities.narrat";
 import main from "./scripts/main.narrat";
 
-export default [beach, callEx, climax, intro, friend, prototype, tasks, utilities, main];
+export default [beach, callEx, climax, end_ex, end_friend, end_mother, intro, friend, prototype, tasks, utilities, main];
