f8da99fa7384b49c91f386ddc09daaca40202a60
KlinsmannHengles <klinsmannhengles@gmail.com>
2023-10-01 19:08:39 -0300

    climax almost finished

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,5 +1,6 @@
 import beach from "./scripts/beach.narrat";
 import callEx from "./scripts/callEx.narrat";
+import climax from "./scripts/climax.narrat";
 import intro from "./scripts/intro.narrat";
 import friend from "./scripts/friend.narrat"
 import prototype from "./scripts/prototype.narrat";
@@ -7,4 +8,4 @@ import tasks from "./scripts/tasks.narrat";
 import utilities from "./scripts/utilities.narrat";
 import main from "./scripts/main.narrat";
 
-export default [beach, callEx, intro, friend, prototype, tasks, utilities, main];
+export default [beach, callEx, climax, intro, friend, prototype, tasks, utilities, main];
