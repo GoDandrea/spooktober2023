960d85e59988472bb8a5ce6a134532fbc19b95b0
KlinsmannHengles <klinsmannhengles@gmail.com>
2023-10-02 00:11:10 -0300

    fixed friend dialogue, mirror scene and scripts

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,13 +1,14 @@
 import beach from "./scripts/beach.narrat";
 import callEx from "./scripts/callEx.narrat";
 import climax from "./scripts/climax.narrat";
-import end_ex from "./scripts/end_ex.narrat"
-import end_friend from "./scripts/end_friend.narrat"
-import end_mother from "./scripts/end_mother.narrat"
+import end_ex from "./scripts/end_ex.narrat";
+import end_friend from "./scripts/end_friend.narrat";
+import end_mother from "./scripts/end_mother.narrat";
 import intro from "./scripts/intro.narrat";
-import friend from "./scripts/friend.narrat"
+import friend from "./scripts/friend.narrat";
+import mirror from "./scripts/mirror.narrat";
 import tasks from "./scripts/tasks.narrat";
 import utilities from "./scripts/utilities.narrat";
 import main from "./scripts/main.narrat";
 
-export default [beach, callEx, climax, end_ex, end_friend, end_mother, intro, friend, tasks, utilities, main];
+export default [beach, callEx, climax, end_ex, end_friend, end_mother, intro, friend, mirror, tasks, utilities, main];
